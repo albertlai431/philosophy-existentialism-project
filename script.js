e9f3bba56763df3f8ef362408cddd8c51377dc28
Albert <albertlai631@outlook.com>
2020-12-22 13:58:23 -0500

    fix: change name and remove unused code

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-function checkAnswer(){
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    if (username == "349865215" && password=="tf697asm")
-        location = 'accounts/idu-wana.html';
-    else
-        alert("Incorrect username or password! Please try again.")
-    return false;
+function checkAnswer() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  if (username == "349865215" && password == "tf697asm")
+    location = "accounts/sally.html";
+  else alert("Incorrect username or password! Please try again.");
+  return false;
 }
