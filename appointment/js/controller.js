b8db89da8c2aa21d19e45af5fdde306ef05b7bac
Albert <albertlai631@outlook.com>
2020-12-23 17:58:36 -0500

    fix: remove cdn and unused js

--- appointment/js/controller.js
+++ appointment/js/controller.js
@@ -2,14 +2,4 @@ document.body.onload = function () {
   var chatRoom = new Chat();
   var owner = "Zoey";
   chatRoom.setOwner(owner);
-  var chatSimulator = new MessagingSim(chatRoom);
-  document.forms["msgForm"].onsubmit = function (event) {
-    chatRoom.displayMsg({
-      name: owner,
-      text: document.forms["msgForm"].message.value,
-      imageUrl: "images/zoey.jpg",
-    });
-    document.forms["msgForm"].message.value = "";
-    event.preventDefault();
-  };
 };
