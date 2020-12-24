91db9c577f687f13906dd30345caacb550b37a8d
Albert <albertlai631@outlook.com>
2020-12-24 08:34:45 -0500

    feat: add felicity

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function checkAnswer() {
   var password = document.getElementById("password").value;
   if (username == "349865215" && password == "tf697asm")
     location = "accounts/sally.html";
+  else if (username == "072659842" && password == "pk420qqq")
+    location = "accounts/felicity.html";
   else alert("Incorrect username or password! Please try again.");
   return false;
 }
